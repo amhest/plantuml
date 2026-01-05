f216d679f82d186f0cfc081bd035a9f0e80b03bd
Jake Wardon <jwardon@synchron.com>
2026-01-05 11:18:49 -0500

    Add CI to publish synchron.sphinxcontrib-plantuml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 long_desc = open('README.rst').read()
 
 setup(
-    name='sphinxcontrib-plantuml',
+    name='synchron.sphinxcontrib-plantuml',
     version='0.31',
     url='https://github.com/sphinx-contrib/plantuml/',
     download_url='https://pypi.python.org/pypi/sphinxcontrib-plantuml',
